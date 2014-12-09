7d38a811dd120906e3a60e3967667cc1da38b71a
loic <lgevrey@oxya.com>
2014-12-09 12:00:16 +0100

    Improve conf ui

--- desktop/php/nest.php
+++ desktop/php/nest.php
@@ -48,7 +48,7 @@ $eqLogics = eqLogic::byType('nest');
             <div class="col-sm-6">
                 <form class="form-horizontal">
                     <fieldset>
-                        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}</legend>
+                        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
                         <div class="form-group">
                             <label class="col-sm-4 control-label">{{Nom de l'équipement nest}}</label>
                             <div class="col-sm-6">
