03af7dd3954cee9ad08e6d259e3e9168125c3760
Loïc <loic@jeedom.com>
2016-05-23 19:14:43 +0200

    Improve security

--- core/ajax/nest.ajax.php
+++ core/ajax/nest.ajax.php
@@ -16,22 +16,23 @@
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
 
+header('Content-Type: application/json');
 try {
-    require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-    include_file('core', 'authentification', 'php');
+	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+	include_file('core', 'authentification', 'php');
 
-    if (!isConnect('admin')) {
-        throw new Exception(__('401 - Accès non autorisé', __FILE__));
-    }
+	if (!isConnect('admin')) {
+		throw new Exception(__('401 - Accès non autorisé', __FILE__));
+	}
 
-    if (init('action') == 'syncWithNest') {
-        nest::syncWithNest();
-        ajax::success();
-    }
+	if (init('action') == 'syncWithNest') {
+		nest::syncWithNest();
+		ajax::success();
+	}
 
-    throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
-    /*     * *********Catch exeption*************** */
+	throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
+	/*     * *********Catch exeption*************** */
 } catch (Exception $e) {
-    ajax::error(displayExeption($e), $e->getCode());
+	ajax::error(displayExeption($e), $e->getCode());
 }
 ?>
